a567470af1547121b2c266bce198966a6cf79f42
jan146 <anurjan@gmail.com>
2023-08-18 18:40:50 +0200

    Rename virtio -> vfio

---
+++ vfio/attach_input_devices.sh
@@ -0,0 +1,18 @@
+DOMAIN="win10"
+if [ $# -gt 0 ]; then
+	DOMAIN=$1
+fi
+VFIO_DIR="$HOME/vfio"
+echo "Domain: $DOMAIN"
+
+for file in $VFIO_DIR/xml/*.xml
+do
+	sudo virsh detach-device --domain $DOMAIN --file "$file"
+	sudo virsh attach-device --domain $DOMAIN --file "$file" 
+done
+
+#sudo virsh detach-device --domain $DOMAIN --file $VFIO_DIR/xml/input1.xml 
+#sudo virsh attach-device --domain $DOMAIN --file $VFIO_DIR/xml/input1.xml 
+#sudo virsh detach-device --domain $DOMAIN --file $VFIO_DIR/xml/input2.xml
+#sudo virsh attach-device --domain $DOMAIN --file $VFIO_DIR/xml/input2.xml
+
